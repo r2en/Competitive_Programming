fe745d0a556ae658a10235e54b936e4e2ab57f9b
xxxHAL <s12161te@gmail.com>
2017-03-02 23:55:26 +0900

    yukicoder

---
+++ yukicoder/289.cpp
@@ -0,0 +1,16 @@
+/*string 取得後 位置文字ずつ精査 数字0 ~ 9の文字列ならそれを違う配列に入れて数値変換 最後足し算*/
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void){
+    string S;
+    cin >> S;
+
+    for(int i = 0;i < S.length(); i++){
+        /*if( (S[i] == "str")||(S[i] == 1)||(S[i] == 2)||(S[i] == 3)||(S[i] == 4)||(S[i] == 5)||(S[i] == 6)||(S[i] == 7)||(S[i] == 8)||(S[i] == 9)){
+            cout << "OK";
+        }*/
+    }
+    return(0);
+}
