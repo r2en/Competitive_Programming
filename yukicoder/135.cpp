9ced384703a9b3582c09e0eae373813a07b2f95a
xxxHAL <s12161te@gmail.com>
2017-04-14 20:22:29 +0900

    yukicoder

--- yukicoder/135.cpp
+++ yukicoder/135.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cstdlib>
+#include <vector>
 using namespace std;
-using ll = long long;
 
 int main(){
-    ll N = 0; ll tmp = 0; ll mini = 1e9;
-    cin >> N; vector<ll>X(N);
-    for(int i = 0; i < N; ++i){ cin >> X[i]; }
+    int N; scanf("%d",&N);
+    vector<int>X(N);
+    for(int i = 0; i < N; ++i){ scanf("%d",&X[i]); }
     sort(X.begin(),X.end());
     X.erase(unique(X.begin(),X.end()),X.end());
-    for(int i = 0; i < X.size(); ++i){ mini = min(mini, X[i+1]-X[i]); }
-    cout << ( mini == 1e9 ? 0 : mini) << endl;
+    int res = 1e9;
+    for(int i = 0; i < X.size() - 1; ++i){
+        res = min(res,X[i+1]-X[i]);
+    }
+    cout << (res == 1e9 ? 0 : res) << endl;
     return 0;
 }
